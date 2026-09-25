c8e76b9aa07ec7ef6352f593ffe4965ac8e9ad46
asd <macintoshkrug@gmail.com>
2026-09-25 21:18:25 +0300

    setup for gym

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -3,7 +3,7 @@ use agb::{fixnum::Num, include_aseprite, include_background_gfx};
 pub type Fixed = Num<i32, 8>;
 
 pub const VEL: i32 = 10;
-pub const INITIAL_HEALTH: i32 = 3;
+pub const INITIAL_HEALTH: i32 = 1;
 pub const TURN_ON_BGM: bool = false;
 
 include_background_gfx!(
